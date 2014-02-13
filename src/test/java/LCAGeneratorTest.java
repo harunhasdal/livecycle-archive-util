6a2e591cadbf3379d1e6e7a112e0b10c376d3fb9
Harun Hasdal <harun.hasdal@rma-consulting.com>
2014-02-13 13:52:09 +0000

    Add test for invalid directory

--- src/test/java/LCAGeneratorTest.java
+++ src/test/java/LCAGeneratorTest.java
@@ -2,12 +2,25 @@ import com.github.harunhasdal.LCAGenerator;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import java.io.File;
+
 public class LCAGeneratorTest {
 
 	@Test
 	public void throwsExceptionWhenNoBaseDirectorySpecified() throws Exception {
 		try {
 			new LCAGenerator(null);
+			Assert.fail("Should throw IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+
+		}
+	}
+
+
+	@Test
+	public void throwsExceptionWhenBaseDirectoryInvalid() throws Exception {
+		try {
+			new LCAGenerator(new File("NonExistent"));
 			Assert.fail("Should throw IllegalArgumentException");
 		} catch (IllegalArgumentException e) {
 
